35966110a594d423244c4ec24251a17bf257e5d3
GL.iNet-Hongjian.Zhang <hongjian.zhang@gl-inet.com>
2021-01-19 20:34:50 +0800

    fix watchdog drives

--- src/gl_hw_wdt.c
+++ src/gl_hw_wdt.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
-#include <linux/gpio.h> 
+#include <linux/gpio/consumer.h>
+#include <linux/gpio.h>
 #include <linux/hrtimer.h>
 #include <linux/fs.h>
 #include <linux/interrupt.h>
@@ -9,122 +10,153 @@
 #include <linux/module.h>
 #include <linux/device.h>
 
-#define HW_WDI	2//watchdog gpio WDI
+#include <linux/platform_device.h>
+#include <linux/miscdevice.h>
+#include <linux/of_platform.h>
+#include <linux/fb.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+
 #define HW_DWI_INT_NS 1000000000 //  feed dog period,100ms = 100000000ns
 static struct hrtimer hw_wdt_timer;
 static ktime_t hw_wdt_timeout;
 
 
-static int enable = 1;//驱动加载时，默认开启喂狗功能，待应用层打开时关闭
-static int __init hw_wdt_drv_init(void);
-static void __exit hw_wdt_drv_exit(void);
+static int enable = 1;//驱动加载时，默认开启喂狗
 // static int wdt_open(struct inode *inode, struct file *file)
 // {
 // 	enable = 0;//关闭定时器喂狗功能
 // 	gpio_direction_output(HW_WDT_CTL,0);
 // 	return 0;
 // }
- 
+
 // static unsigned int wdt_status=0x00;//记录引脚状态，每调用一次write时进行"非"操作一次
- 
+
 static ssize_t wdt_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
 {
-	if((buf[0] == '1' || buf[0] == 1) && enable == 0) {
-		enable = 1;
-		hrtimer_start(&hw_wdt_timer, hw_wdt_timeout, HRTIMER_MODE_REL);
-		printk("enable hardware watchdog\n");
-	}
-	else if((buf[0] == '0' || buf[0] == 0) && enable == 1) {
-		enable = 0;
-		printk("disable hardware watchdog\n");
-	}
+    if((buf[0] == '1' || buf[0] == 1) && enable == 0) {
+        enable = 1;
+        hrtimer_start(&hw_wdt_timer, hw_wdt_timeout, HRTIMER_MODE_REL);
+        printk("enable hardware watchdog\n");
+    } else if((buf[0] == '0' || buf[0] == 0) && enable == 1) {
+        enable = 0;
+        printk("disable hardware watchdog\n");
+    }
 
-	return 0;
+    return 0;
 }
- 
-static struct file_operations hw_wdt_fops={
-	.owner = THIS_MODULE,
-	// .open  = wdt_open,
-	.write = wdt_write,
+
+static struct file_operations hw_wdt_fops= {
+    .owner = THIS_MODULE,
+    // .open  = wdt_open,
+    .write = wdt_write,
 };
 
+static  struct miscdevice  g_tmisc = {
+    .minor 	=	MISC_DYNAMIC_MINOR,
+    .name 	=	"hw_wdt",
+    .fops 	=	&hw_wdt_fops,
+};
+
+static unsigned int feed_dog_gpio = 0xff;
+static unsigned int dog_en_gpio = 0xff;
+static unsigned int feed_dog_interval = HW_DWI_INT_NS;
 static enum hrtimer_restart hw_wdt_timeout_handle(struct hrtimer* timer)
 {
-	enum hrtimer_restart ret = HRTIMER_RESTART;
-	static int gpio_status = 0;
-	static int a = 0;
+    enum hrtimer_restart ret = HRTIMER_RESTART;
+    static int gpio_status = 0;
+    static int a = 0;
 
-	a++;
-	if(a > 10) {
-	 a=0;
-	}
+    a++;
+    if(a > 10) {
+        a=0;
+    }
 
-	ktime_t current_time = ktime_get();
+    ktime_t current_time = ktime_get();
 
-	hrtimer_forward(&hw_wdt_timer, current_time, hw_wdt_timeout);
+    hrtimer_forward(&hw_wdt_timer, current_time, hw_wdt_timeout);
 
-	gpio_status = !gpio_status;
-	gpio_set_value(HW_WDI, gpio_status);
-//	gpiod_set_value(gpio_to_desc(HW_WDI),gpio_status);
-	
-	if(enable) {
-		ret = HRTIMER_RESTART;
-	}
-	else {
-		ret = HRTIMER_NORESTART;
-	}
-	
+    gpio_status = !gpio_status;
+    gpiod_set_value(gpio_to_desc(feed_dog_gpio),gpio_status);
 
-	return ret;
+    if(enable) {
+        ret = HRTIMER_RESTART;
+    } else {
+        ret = HRTIMER_NORESTART;
+    }
+
+
+    return ret;
 }
 
 
-static int major=0;
-static struct class *drv_class;
- 
-static int __init hw_wdt_drv_init(void)
+static int hw_wdt_probe(struct platform_device *pdev)
 {
-	printk("hw_wdt enter probe\n");
+    printk("hw_wdt enter probe\n");
 
-	hw_wdt_timeout = ktime_set(0, HW_DWI_INT_NS);
-	hrtimer_init(&hw_wdt_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	hw_wdt_timer.function = &hw_wdt_timeout_handle;
-	
-/*	if( gpio_request(HW_WDI, "hw_wdi") != 0 ){
-		printk("gpio_request gpio%d error\n",HW_WDI);
-		return 0;
-	}
-	
-	if( gpio_direction_output(HW_WDI, 1) != 0 ){
-		printk("gpio_direction_output gpio%d error\n",HW_WDI);
-		gpio_free(HW_WDI);
-		return 0;
-	}
-*/
-	hrtimer_start(&hw_wdt_timer, hw_wdt_timeout, HRTIMER_MODE_REL);
 
-	// devno = MKDEV(major,minor);
-	major = register_chrdev(major,"hw_wdt",&hw_wdt_fops);
-	drv_class = class_create(THIS_MODULE,"hw_wdt");
-	device_create(drv_class,NULL,MKDEV(major,0),NULL,"hw_wdt");
-	return 0;
+    if(!of_property_read_u32(pdev->dev.of_node,"feed_dog_gpio",&feed_dog_gpio)) {
+        printk("feed dog gpio = gpio%d \n",feed_dog_gpio);
+        if( gpio_request(feed_dog_gpio, "feed_dog") != 0 ) {
+            printk("gpio_request gpio%d error\n",feed_dog_gpio);
+        }
+
+        if( gpiod_direction_output(gpio_to_desc(feed_dog_gpio), 1) != 0 ) {
+            printk("gpio_direction_output gpio%d error\n",feed_dog_gpio);
+        }
+        gpiod_export(gpio_to_desc(feed_dog_gpio),1);
+    }
+
+    if(!of_property_read_u32(pdev->dev.of_node,"dog_en_gpio",&dog_en_gpio)) {
+        printk("watchdog enable gpio = gpio%d \n",dog_en_gpio);
+        if( gpio_request(dog_en_gpio, "dog_en") != 0 ) {
+            printk("gpio_request gpio%d error\n",dog_en_gpio);
+        }
+
+        if( gpiod_direction_output(gpio_to_desc(dog_en_gpio), 1) != 0 ) {
+            printk("gpio_direction_output gpio%d error\n",dog_en_gpio);
+        }
+        gpiod_export(gpio_to_desc(dog_en_gpio),1);
+    }
+    if(!of_property_read_u32(pdev->dev.of_node,"feed_dog_interval",&feed_dog_interval)) {
+        printk("feed watchdog interval %d ns\n",feed_dog_interval);
+        hw_wdt_timeout = ktime_set(0, feed_dog_interval);
+        hrtimer_init(&hw_wdt_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+        hw_wdt_timer.function = &hw_wdt_timeout_handle;
+    }
+
+
+    hrtimer_start(&hw_wdt_timer, hw_wdt_timeout, HRTIMER_MODE_REL);
+
+    misc_register(&g_tmisc);
+    return 0;
 }
- 
-static void __exit hw_wdt_drv_exit(void)
+
+static int hw_wdt_remove(struct platform_device *pdev)
 {
-	printk("enter remove\n");
-	hrtimer_cancel(&hw_wdt_timer);
-//	gpio_free(HW_WDI);
-	device_destroy(drv_class,MKDEV(major,0));
-	class_destroy(drv_class);
-	unregister_chrdev(major,"hw_wdt");
+    printk("enter remove\n");
+    hrtimer_cancel(&hw_wdt_timer);
+    misc_deregister(&g_tmisc);
 }
+
+static const struct of_device_id of_hw_wdt_match[] = {
+    { .compatible = "hw_wdt", },
+    {},
+};
+
+static struct platform_driver hw_wdt_driver = {
+    .probe		= hw_wdt_probe,
+    .remove		= hw_wdt_remove,
+    .driver		= {
+        .name	= "hw_wdt",
+        .of_match_table = of_hw_wdt_match,
+    },
+};
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("deng");
 MODULE_DESCRIPTION("gl hardware watchdog");
 MODULE_VERSION("0.1");
 
-module_init(hw_wdt_drv_init);
-module_exit(hw_wdt_drv_exit);
- 
+module_platform_driver(hw_wdt_driver);
+
